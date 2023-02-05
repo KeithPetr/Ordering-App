0c6df0e7ec8d982de54d97390cc9023ac3cdab9b
KeithPetr <keith.petryshyn@gmail.com>
2023-02-05 18:55:34 -0500

    Add items and total the price
    Able to now add individual items in the order summary and keep track of the total

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import { menuArray } from "/data.js"
+import { menuArray } from "/data.js";
 
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
+const orderEl = document.getElementById("order");
+const orderTitleEl = document.getElementById("order-title");
+const totalPriceEl = document.getElementById("total-price");
+const completeOrderBtn = document.getElementById("complete-order-btn");
+let orderArray = [];
+let totalPrice = [];
 
 // this is the render function that iterates through each object in the menu array
 // each object is called 'value' and dot notation can be used to access the different
@@ -8,10 +14,33 @@ const menu = document.getElementById("menu")
 // template literals are used to allow for html tags easier and allow for easy access
 // to those js variables within the html
 
+// when you click on the plus sign, an item needs to be added to the order summary
+// it should then show the total and display the complete order button
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.added) {
+    handleAddItemClick(menuArray[e.target.dataset.added]);
+  }
+  renderOrderSummary();
+  priceTotal()
+});
+
+// function to push an individual item that has it's corresponding
+// plus sign clicked on
+
+function handleAddItemClick(item) {
+  orderArray.push(item);
+  totalPrice.push(item.price);
+}
+
+// this function loops through the menuArray and assigns each object in that array to that
+// array to value. I then use dot notation to access the values at each key within
+// each object and apply them as JS to the inner HTML using template literals
+
 function renderMenu() {
-    let menuString = ''
-    for (let value of menuArray) {
-        menuString += `
+  let menuString = "";
+  for (let value of menuArray) {
+    menuString += `
         <div class="item-section" id="item-section">
             <div class="item">
                 <div class="emoji" id="emoji">${value.emoji}</div>
@@ -21,11 +50,45 @@ function renderMenu() {
                     <div class="price">$${value.price}</div>
                 </div>
             </div>
-            <img src="images/plus-circle.svg" class="plus-circle" alt="add item symbol">
+            <img 
+            src="images/plus-circle.svg" id="plus-circle" class="plus-circle" 
+            alt="add item symbol" data-added="${value.id}"
+            >
         </div>
-        `
-    }
-    menu.innerHTML = menuString
+        `;
+  }
+  menu.innerHTML = menuString;
 }
 
-renderMenu()
+function renderOrderSummary() {
+  let orderString = "";
+  orderEl.classList.remove("hidden");
+  orderTitleEl.classList.remove("hidden");
+  completeOrderBtn.classList.remove("hidden");
+  for (let item of orderArray) {
+    orderString += `
+            <div id="order-item-section" class="order-item-section">
+                <div class="order-item">
+                    <div class="order-name">${item.name}</div>
+                    <div class="remove-btn">remove</div>
+                </div>
+                <div class="order-price">$${item.price}</div>
+            </div>
+        `;
+  }
+  orderEl.innerHTML = orderString;
+}
+
+function priceTotal() {
+    let priceString = "";
+    let total = totalPrice.reduce((a, b) => a + b, 0);
+    priceString = `
+    <div class="total-price-section">
+          <div class="total-price-title">Total Price:</div>
+          <div class="total-price-amount">$${total}</div>
+    </div>
+    `
+    totalPriceEl.innerHTML = priceString
+}
+
+renderMenu();
